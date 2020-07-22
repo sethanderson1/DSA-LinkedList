e77aae580d9aab807b7415ddfc0fc0c37d3c13c7
Seth Anderson <sdande02@gmail.com>
2020-07-21 22:21:03 -0700

    finished drills

--- drills.js
+++ drills.js
@@ -1,56 +1,311 @@
-// In these drills, you'll practice creating a linked list, implementing its core, and some supplemental operations. You will also use your linked list to solve interview questions. Don't forget to assess the big O for each of these exercises. Start each problem by stating 1 or more sample inputs and outputs.
+class _Node {
+    constructor(value, next) {
+        this.value = value;
+        this.next = next;
+    };
+};
 
-// Create a linked list class
-// Walk through the linked list code in the curriculum and understand it well. Then write a linked list class and its core functions (insertFirst, insertLast, remove, find) from scratch.
+//Create a linked list class
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    };
+    // insert first node
+    insertFirst(item) {
+        this.head = new _Node(item, this.head);
+    };
+
+    insertLast(item) {
+        if (this.head === null) {
+            this.insertFirst(item);
+        } else {
+            // begin at start node
+            let tempNode = this.head;
+            while (tempNode.next !== null) {
+                // jump to each subsequent node until next is null
+                tempNode = tempNode.next;
+            }
+            tempNode.next = new _Node(item, null);
+        };
+    };
+
+    find(item) {
+        // Start at the head
+        let currNode = this.head;
+        // If the list is empty
+        if (!this.head) {
+            return null;
+        }
+        // Check for the item
+        while (currNode.value !== item) {
+            /* Return null if it's the end of the list 
+                     and the item is not on the list */
+            if (currNode.next === null) {
+                return null;
+            } else {
+                // Otherwise, keep looking
+                currNode = currNode.next;
+            }
+        }
+        // Found it
+        return currNode;
+    }
+
+
+    remove(item) {
+        // If the list is empty
+        if (!this.head) {
+            return null;
+        }
+        // If the node to be removed is head, make the next node head
+        if (this.head.value === item) {
+            this.head = this.head.next;
+            return;
+        }
+        // Start at the head
+        let currNode = this.head;
+        // Keep track of previous
+        let previousNode = this.head;
+
+        while (currNode !== null && currNode.value !== item) {
+            // Save the previous node
+            previousNode = currNode;
+            currNode = currNode.next;
+        }
+        if (currNode === null) {
+            console.log("Item not found");
+            return;
+        }
+        previousNode.next = currNode.next;
+    }
+
+
+
+    insertBefore(key, item) {
+        // Start at the head
+        let currNode = this.head;
+        // If the list is empty
+        if (!this.head) {
+            return null;
+        }
+        // search until find key 
+        while (currNode.value !== key) {
+            if (currNode.next === null) {
+                return null;
+            } else {
+                // else, keep going
+                currNode = currNode.next;
+            }
+        }
+        // key found, so have item point to it
+        let newNode = new _Node(item, currNode.next);
+        currNode.next = newNode;
+    }
+
+    insertAfter(item, key) {
+        if (this.head === null) {
+            this.insertFirst(item)
+        }
+        else {
+            let currNode = this.head
+            // while not at end and havent found key yet
+            while ((currNode !== null) && (currNode.value !== key)) {
+                currNode = currNode.next
+            }
+            if (currNode === null) {
+                this.insertLast(item)
+                return
+            }
+            // found key so have point to item
+            currNode.next = new _Node(item, currNode.next)
+        }
+    }
+
+    insertAt(item, idx) {
+        if (!this.head) {
+            return null
+        }
+        if (idx > this.length - 1) {
+            return null
+        }
+        let currNode = this.head
+        let prevNode = this.head
+
+        for (let i = 0; i < idx - 1; i++) {
+            prevNode = currNode
+            currNode = currNode.next
+        }
+        let newNode = new _Node(item, currNode)
+        prevNode.next = newNode
+        this.length++
+    }
+
+
+    findLast() {
+        let currNode = this.head;
+        // If the list is empty
+        if (!this.head) {
+            return null;
+        }
+        // Check for the item
+        while (currNode.next !== null) {
+            if (currNode.next === null) {
+                return currentNode;
+            } else {
+                // Otherwise, keep looking
+                currNode = currNode.next;
+            }
+        }
+    }
+
+}
+
+
+const display = linkedList => {
+    console.log('linkedList', linkedList)
+};
+
+const size = linkedList => {
+    let currentNode = linkedList.head;
+    let count = 0;
+    while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+        count++;
+    }
+    return count;
+};
+
+const isEmpty = linkedList => {
+    if (linkedList.head === null) {
+        return false;
+    } else {
+        return true;
+    }
+};
 
 // Creating a singly linked list
-// Write a function main. Within the function, using the linked list class above, create a linked list with the name SLL and add the following items to your linked list: Apollo, Boomer, Helo, Husker, Starbuck.
-// Add Tauhida to the list.
-// Remove Husker from the list.
-// Implement a function called insertBefore() in the class that inserts a new node before a given node containing a key.
-// Implement a function called insertAfter() in the class that inserts a new node after a node containing the key.
-// Implement a function called insertAt() that inserts an item at a specific position in the linked list.
-// Add Athena before Boomer using your insertBefore() function.
-// Add Hotdog after Helo using the insertAfter() method.
-// Using the insertAt() method insert Kat in the 3rd position of the list.
-// Remove Tauhida from the list.
-// Supplemental functions for a linked list
-// Implement the following functions that operate on your linked list class. Note that these should be free functions instead of methods of the linked list class, so implement them outside the linked list class. Test each function using the list created in exercise 1.
 
-// display: displays the linked list
-// size: returns the size of the linked list
-// isEmpty: finds if the list is empty or not (without using the size() function)
-// findPrevious: finds the node before the item you are looking for
-// findLast: returns the last node in the linked list
-// Mystery program
-// Analyze the following function (without running it in an IDE) to determine what problem it is trying to solve. What is the time complexity of this algorithm?
+const main = () => {
+    const SLL = new LinkedList();
 
-// function WhatDoesThisProgramDo(lst) {
-//     let current = lst.head;
-//     while (current !== null) {
-//         let newNode = current;
-//         while (newNode.next !== null) {
-//             if (newNode.next.value === current.value) {
-//                 newNode.next = newNode.next.next;
-//             }
-//             else {
-//                 newNode = newNode.next;
-//             }
-//         }
-//         current = current.next;
+    SLL.insertFirst('Starbuck');
+    SLL.insertFirst('Husker');
+    SLL.insertFirst('Helo');
+    SLL.insertFirst('Boomer');
+    SLL.insertFirst('Apollo');
+
+    SLL.insertLast("Tauhida");
+
+    SLL.remove("Boomer");
+    SLL.find("Helo");
+    SLL.insertBefore("Boomer", "Athena");
+    SLL.insertAfter('Hotdog', 'Helo');
+    SLL.insertAt('Kat', 3);
+    SLL.remove("Tauhida");
+
+    display(SLL);
+    console.log('isEmpty(SLL)', isEmpty(SLL))
+    console.log('size(SLL)', size(SLL))
+};
+
+main();
+console.log('main()', main())
+
+// Mystery Program
+
+function WhatDoesThisProgramDo(lst) {
+    let current = lst.head;
+    while (current !== null) {
+        let newNode = current;
+        while (newNode.next !== null) {
+            if (newNode.next.value === current.value) {
+                newNode.next = newNode.next.next;
+            } else {
+                newNode = newNode.next;
+            }
+        }
+        current = current.next;
+    }
+}
+
+// it removes duplicates
+
+// // reverse a list 
+
+// function reverse(head) {
+//     let currNode = head;
+//     let prevNode, nextNode
+
+//     while (currNode) {
+//         nextNode = currNode.next;
+//         currNode.next = prevNode;
+//         prevNode = currNode;
+//         currNode = nextNode;
 //     }
+//     return prevNode;
 // }
-// Reverse a list
-// Write an algorithm to reverse a linked list. The time complexity of your algorithm should be linear (O(n)). For this exercise, notice we are not asking you just to print the linked list in reverse or use another linked list to store the value in reverse order. Your program should reverse the direction of a given singly linked list. In other words, all pointers should point backward. BONUS: Solve this problem using both recursive and iterative algorithms.
+
+// Recursive
+const reverse = head => {
+    if (!head || !head.next) {
+        return head;
+    }
+
+    let tmp = reverse(head.next);
+    head.next.next = head;
+    head.next = undefined;
+    return tmp;
+};
 
 // 3rd from the end
-// Write an algorithm to find the 3rd element from the end of a linked list. Note You may be tempted to add a length property to your linked list class. The length property is not a typical property of linked list, therefore don't make any modification to the linked list class that is provided to you.
+
+const find3rd = list => {
+    let currNode = this.head;
+    if (!this.head) {
+        return null;
+    }
+    while (currNode.value !== null) {
+        if (currNode.next.next === null) {
+            return currNode;
+        } else {
+            currNode = currNode.next;
+        }
+    }
+};
 
 // Middle of a list
-// Write an algorithm to find the middle element of a linked list. Note You may be tempted to add a length property to your linked list class. The length property is not a typical property of linked list, therefore don't make any modification to the linked list class that is provided to you. Also, finding the size of the linked list using the size() function and dividing it by half will not find the correct middle of the linked list. So, don't use either of these approaches.
+//**** */
+const middleList = head => {
+    let double = head
+    let single = head
 
-// Cycle in a list
-// Write an algorithm to find whether a linked list has a cycle (i.e., whether a node in the list has its next value pointing to an earlier node in the list). For this exercise, create a linked list with the name CycleList. Be sure to insert nodes in the list so that it has a cycle. Then test your program with a cycleList function.
+    while (double.next !== null && double.next.next !== null) {
+        double = double.next.next
+        single = single.next
+    }
+
+    return single.value
+}
+
+
+// Cycle in a List
+
+const cycleList = list => {
+    let currNode = list.head;
+    if (!head) {
+        return null;
+    }
+    while (currNode.next !== null) {
+        if (currNode.next === null) {
+            return false;
+        } else {
+            currNode = currNode.next;
+        }
+    }
+};
 
 // Sorting a list
-// Write an algorithm that will sort a given linked list. For example given a list such as 3->2->5->7->1, your program will output the sorted version of this list which will be 1->2->3->5->7. You may not use another list or any other data structure such as an array to store the data.
+//**** */
+// const sortList = linkedList => {
+// };
